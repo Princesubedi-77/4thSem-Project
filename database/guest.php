72d2b44e120ddf1c9fe557451ab1b181c79ce6c9
Princesubedi-77 <subedi7prince@gmail.com>
2026-07-28 12:38:21 +0200

    next db

---
+++ database/guest.php
@@ -0,0 +1,18 @@
+<?php
+include "database.php";
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_Post["phone"];
+
+$sql = "INSERT INTO guest (name,email,phone)
+        VALUES ('$name','$email','$phone')";
+
+        if($conn->query($sql)){
+            header("location: /hms/page/guest.html");
+            exit();
+        }
+        else {
+            echo " Try again";
+        }
+?>
